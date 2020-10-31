f9815f375a9cb2595856f87f95b3ce20204acca5
BarlowR <robbarlw@gmail.com>
2020-10-31 18:00:38 -0400

    computer's dying

---
+++ outline_path.py
@@ -0,0 +1,16 @@
+import numpy as np
+
+
+
+def OutlinePath(object):
+
+	@staticmethod
+	def outline_outer_image(img, prior_paths):
+		#img is a numpy boolean array of a black and white image, where blac pixels are 1 and white pixels are 0
+		#prior_paths is a list of directed graphs, with each element corresponding to a boundry outline in the original image
+		start_vertex = (0,0)
+		for row in img[1:;]
+			for column in row:
+				if 
+
+

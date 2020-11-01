197f22a4268c88bb7c9b5f6f5d082b4ad8b4056e
BarlowR <robbarlw@gmail.com>
2020-11-01 14:34:24 -0500

    cleaned up outline_path.py and added some additonal functionality. Now working on a path_to_polygon method to approximate paths with simpler polygons

--- outline_path.py
+++ outline_path.py
@@ -6,18 +6,21 @@ import matplotlib.pyplot as plt
 class OutlinePath:
 
 	dir_select = {
+	#based on a 2x2 grid of pixels, determine how to change direction
 		(0, 0, 
-		 1, 0) : 1, 
+		 1, 0) : 1,  #turn left
 
 		(1, 0,
-		 1, 0) : 0,
+		 1, 0) : 0,  #go straight
 
 		(1, 1, 
-		 1, 0) : -1,
+		 1, 0) : -1, #turn right 
 
 		(0, 1,
-		 1, 0) : 1,
+		 1, 0) : 1,  #turn left
 		}
+
+
 
 	vertex_shift = {
 	#key is direction of motion along path; > is 0, ^ is 1, < is 2, v is 3. Element is tuple of vertex x,y index change
@@ -29,56 +32,94 @@ class OutlinePath:
 
 
 
+
+
+		#Recursively finds all paths along black/white borders within the image given, and returns them as a list of directed graphs. 	
 	@staticmethod
-	def outline_outer_image(img):
-		#img is a numpy boolean array of a black and white image, where black pixels are 1 and white pixels are 0
-		#prior_paths is a list of directed graphs, with each element corresponding to a boundry outline in the original image
-		
+	def outline_outer_image(img, path_num = -1, min_path_length = 0):
+		#img is a numpy boolean array of a black and white image, where black pixels are 1 and white pixels are 0. 
+		#path_num defines max number of paths to return. 
+		#min_path_length defines the minimum path length to include in the resulting path list
 
 
 		path = {}
 
+		#Grab top leftmost vertex along a border
 		current_vertex = OutlinePath.find_start_vertex(img)
 
-		if current_vertex is not None:
+
+		# if there exist any pixels to trace and we don't already have enough paths, trace the outline of the path
+		#else, return an empty list
+		if (current_vertex is not None and path_num != 0):
+
+			#setup the path tracing variables
 			first_vertex = current_vertex
 			last_vertex = None
 			current_theta = 0;
 			current_grid = OutlinePath.get_grid(current_vertex, img)
 
+			# our path tracing algorithm only follows outlines with the black pixels on the left side & white pixels on the right, 
+			# this takes our initial vertex and orients our direction such that the black pixels are on the left.
 			for i in range(4):
 				grid = OutlinePath.dir_trans(i, current_grid)
 				if grid in OutlinePath.dir_select:
 					current_theta = i
 					break
 
+			# run this loop while we have a closed loop in our path
 
-			while not (first_vertex == last_vertex):
+			while True:
 
+				#take a 2x2 grid of pixels surrounding our current vertex and rotate it to our path-tracing direction
 				rotated_grid = OutlinePath.dir_trans(current_theta, current_grid)
+
+				#based on the rotated grid, determine what direction to take & update our direction
 				current_theta = (current_theta + OutlinePath.dir_select[rotated_grid])%4
 
+				#determine our next vertex location
 				vert_x, vert_y = current_vertex
 				x_shift, y_shift = OutlinePath.vertex_shift[current_theta]
 				new_vertex = (vert_x + x_shift, vert_y + y_shift)
 
-
+				#add our (current vertex : new vertex) pair to the path directed graph 
 				path[current_vertex] = new_vertex
 
-
+				#move our vertex along and pull the new pixel grid
 				current_vertex = new_vertex
 				current_grid = OutlinePath.get_grid(current_vertex, img)
 				last_vertex = current_vertex
 
 				
-				if (current_vertex in path) and (current_vertex != first_vertex):
-					next_vertex = current_vertex
-					while next_vertex is not None:
-						next_vertex = path.pop(next_vertex, None)
+				# check if we've been at this vertex before. 
+				# If it's our starting vertex, then we exit the loop. 
+				# Otherwise, we've traced an internal loop within our outline, which we don't want. 
+				# If this is the case, we pop all values until we've removed the internal loop.
+				if (current_vertex == first_vertex):
+						break
+				elif (current_vertex in path):
+						next_vertex = current_vertex
+						while next_vertex is not None:
+							next_vertex = path.pop(next_vertex, None)
 
+			# Once we have a closed loop, then we invert all of the pixels within the loop, which removes the outline from the image.
 			inverted_image = OutlinePath.invert_closed_path(path, img)
 			
-			return [path] + OutlinePath.outline_outer_image(inverted_image)
+
+			return_list = []
+
+			# if our path length is long enough, we add it to the list of paths and decremend the number of paths left to add
+			if len(path) > min_path_length:
+				return_list += [path]
+				path_num -= 1
+
+			# Recursively calculate the next outline
+			if path_num > 0:
+				return_list += OutlinePath.outline_outer_image(inverted_image, path_num = path_num, min_path_length = min_path_length)
+			elif path_num < 0:
+				return_list += OutlinePath.outline_outer_image(inverted_image, min_path_length = min_path_length)
+
+			return return_list
+		
 		else: return []
 
 			
@@ -89,7 +130,7 @@ class OutlinePath:
 
 
 		
-
+	#iterate over all pixels until we find a pixel vertex along a black/white border
 	def find_start_vertex(img):
 		rows, columns = img.shape
 		for row_index in range(1,rows):
@@ -101,16 +142,18 @@ class OutlinePath:
 		return None
 		
 
+
+
+	# return a given pixel grid rotated by the given theta direction; 
+	# for a theta input of 2 and a pixel grid of [1,0] the resulting grid would be [0,0] and the tuplle would be (0,0,1,1)
+	#                                            [1,0]                             [1,1]                       
 	@staticmethod
 	def dir_trans(theta, grid):
-		#theta is direction of motion along path; > is 0, ^ is 1, < is 2, v is 3
+		# theta is direction of motion along path; > is 0, ^ is 1, < is 2, v is 3
 		# grid is a tuple of the boolean pixels surrounding the current vertex;
 		#	 for a grid that has pixel positions [0,1] the corresping tuple would be (0,1,2,3) 
-		#                            			 [2,3] 
-		# this method returns the pixel grid rotated by the corresponding theta; 
-		#    for a n input of 2 and a pixel grid of [0,0] the resulting grid would be [0,0] and the tuplle would be (0,0,0,1)
-		#                                           [1,0]                             [0,1]                       
-
+		#                                        [2,3]
+			
 		direction_transform = {
 			0 : (grid[1], grid[3], grid[0], grid[2]),
 			1 : (grid[0], grid[1], grid[2], grid[3]),
@@ -120,6 +163,8 @@ class OutlinePath:
 
 		return direction_transform[theta]
 
+
+	# based on vertex and a bolean image array, return the 2x2 grid of pixel values surrounding the vertex
 	@staticmethod
 	def get_grid(vertex, img):
 		x, y = vertex
@@ -130,6 +175,8 @@ class OutlinePath:
 			img[y][x]      #bottom right
 			)
 	
+	
+	# print a given pixel grid
 	@staticmethod
 	def print_grid(grid):
 		# grid is a tuple of the boolean pixels surrounding the current vertex;
@@ -140,8 +187,11 @@ class OutlinePath:
 		print(grid)
 		return grid
 
+
+	#print a given pixel grid with a given theta
 	@staticmethod
 	def print_grid_theta(theta, grid):
+		# theta
 		# grid is a tuple of the boolean pixels surrounding the current vertex;
 		#	 for a grid that has pixel positions [0,1] the corresping tuple would be (0,1,2,3) 
 		#                            			 [2,3] 
@@ -154,16 +204,18 @@ class OutlinePath:
 		print(grid_direction[theta])
 		return grid
 
+	#take in a PIL image and turn it into b&w image with pixel values in (0,1) and a white pixel border. 
 	@staticmethod
-	def img_to_bool_array(img):
-		img_array = np.asarray(img)
-		bool_array = np.where(img_array< 1, np.zeros(img_array.shape), 1)
-
-		padding = np.zeros((bool_array.shape[0]+2, img_array.shape[1]+2))
+	def img_to_bool_array(img, threshold):
+		#threshold determines the cutoff for black v white pixels, should be between 255 and 0. 200 seems to work well for artifact-y jpgs 
+		img_array = np.asarray(img.convert('L'))
+		bool_array = np.where(img_array < threshold, np.ones(img_array.shape), 0)
+		padding = np.zeros((bool_array.shape[0]+2, bool_array.shape[1]+2))
 		padding[1:bool_array.shape[0]+1, 1:bool_array.shape[1]+1] = bool_array
 		return padding
 
 
+	#given an image and a closed path, invert all pixels within the given path
 	@staticmethod
 	def invert_closed_path(path, img):
 		y_dim, x_dim = img.shape
@@ -182,17 +234,36 @@ class OutlinePath:
 
 		return inverted_image	
 
+	#take a path and scatter plot all nodes. Useful for visualizing the path ontop of the initial image
+	@staticmethod
 	def outline_path(path):
 		for node in path:
 			plt.scatter(node[0]-.5, node[1]-.5, color = 'red')	
 
+	#return n = num_to_keep longest paths in the list of paths. 
+	@staticmethod
+	def cull_paths(paths, num_to_keep):
+		paths.sort(reverse = True, key=len)
+		return paths[:num_to_keep]
+
+
+
 
 if __name__ == "__main__":
 
-	image = Image.open('imageSmall.png')
-	img_array = OutlinePath.img_to_bool_array(image)
-	paths = OutlinePath.outline_outer_image(img_array)
+	image = Image.open('flamingo.jpg')
+	plt.subplot(1,2,1)
+	plt.imshow(image, 'gray')
+	img_array = OutlinePath.img_to_bool_array(image, 200)
+	plt.subplot(1,2,2)
+	plt.imshow(img_array, 'gray')
+
+
+	paths = OutlinePath.outline_outer_image(img_array, min_path_length = 10)
+	paths = OutlinePath.cull_paths(paths, 6)
+	
 	for path in paths:
+		print(len(path))
 		OutlinePath.outline_path(path)
 
 	plt.show()
---
+++ path_to_poly.py
@@ -0,0 +1,104 @@
+import numpy as np
+from PIL import Image
+import matplotlib.pyplot as plt
+
+
+class PathToPoly:
+	@staticmethod
+	def path_to_polygon(path, distance_threshold):
+		#path must have min length of 3
+		
+		error_sum = 0
+		max_error_sum = -1
+		best_approx = {}
+
+		for node in path:
+			#reset our variables
+			minimized_path = path
+			start_node = node
+			P1 = node
+			error_sum = 0
+			closed = False;
+			
+			#iterate through nodes, removing nodes if they are within a certain distance of a line from a prior to a later node
+			while not closed:
+
+				#set our initial mid and end points along the path
+				midP = minimized_path[P1]
+				P2 = minimized_path[midP]
+
+				max_node_distance = 0
+
+				line_error_sum = 0
+
+				# while we haven't exceded our max node to line distance, figure out the sum of distance of points between P1 and P2 
+				while max_node_distance < (distance_threshold**2):
+
+					#reset max_node_distance
+					max_node_distance = 0
+					line_error_sum = 0;
+
+					#iterate aolong all midpoints between P1 and P2
+					while midP != P2:
+
+						#calculate distance_squared from the midpoint to the line (P1 to P2)
+						midP_dist = dist_squared_point_to_line(P1, P2, midP)
+
+						#add this distance squared to the error sum
+						line_error_sum += midP_dist
+
+						#if the error is greater than our previous max error, increase our previous max error
+						if midP_dist > max_node_distance: max_node_distance = midP_dist
+
+						#move the midpoint along to the next vertex on the path
+						midP = minimized_path[midP]
+
+					#set the endpoint to the next node in the path
+					P2 = minimized_path[P2]
+
+					#check if we have a closed path yet
+					if P2 = start_node: 
+						closed = True
+						break
+
+					#set the intermediate point to the node after the start node
+					midP = minimized_path[P1]
+				
+				#once we exit from the while loop (sqrt(max_node_distance) has excedded the threshold), remove all nodes between P1 and P2
+				while midP != P2:
+					minimized_path.pop(midP)
+					midP = minimized_path[midP]
+
+				#increase our path error sum by our line error sum
+				error_sum += line_error_sum
+
+				#set our new starting node
+				P1 = P2
+
+			#once we have a closed loop, check if our total error is less than the previous approximations. 
+			#if it is, save the new minimized total error and the minimized path network
+			if error_sum < max_error_sum or max_error_sum == -1:
+				max_error_sum = error_sum
+				best_approx = minimized_path
+		print(max_error_sum)
+		return (best_approx)
+				
+
+
+	#h = 2A/b
+	#h^2 = (2A)^2 / b^2
+	#https://en.wikipedia.org/wiki/Distance_from_a_point_to_a_line#Line_defined_by_two_points
+
+	@staticmethod
+	def dist_squared_point_to_line(P1, P2, midP):
+
+
+		x0, y0 = midP
+		x1, y1 = P1
+		x2, y2 = P2
+
+		area_squared_of_triangle = (x0*(y2-y1) - y0*(x2-x1) + x2*y1 - y2*x1)**2
+		distance_squared_p1_to_p2 = (y2-y1)**2 + (x2-x1)**2
+
+		return area_squared_of_triangle/distance_squared_p1_to_p2
+
